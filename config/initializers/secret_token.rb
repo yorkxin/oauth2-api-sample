3f9b421b423de5c203e23acfdd71bf34411d9cf7
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 19:24:40 +0800

    init project with Can't initialize a new Rails application within the directory of another, please change to a non-Rails directory first. Type 'rails' for help.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oauth2ApiSample::Application.config.secret_key_base = '6b1488355a0815e692afc814b50331822ee43fcf5bfd68d89327d805b97ce60ec0f093077a1e4baecfe36c9720ec0cb76dc85de6ae6e0b595579b933f25ff6bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oauth2ApiSample::Application.config.session_store :cookie_store, key: '_oauth2-api-sample_session'
