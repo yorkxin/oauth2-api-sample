1f893e52154a33cab1d31744f10a17e12824f104
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 21:05:22 +0800

    create OAuth 2 tables with `rails generate doorkeeper:migration`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009122554) do
+ActiveRecord::Schema.define(version: 20131009130122) do
+
+  create_table "oauth_access_grants", force: true do |t|
+    t.integer  "resource_owner_id",              null: false
+    t.integer  "application_id",                 null: false
+    t.string   "token",                          null: false
+    t.integer  "expires_in",                     null: false
+    t.string   "redirect_uri",      limit: 2048, null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "revoked_at"
+    t.string   "scopes"
+  end
+
+  add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true
+
+  create_table "oauth_access_tokens", force: true do |t|
+    t.integer  "resource_owner_id"
+    t.integer  "application_id",    null: false
+    t.string   "token",             null: false
+    t.string   "refresh_token"
+    t.integer  "expires_in"
+    t.datetime "revoked_at"
+    t.datetime "created_at",        null: false
+    t.string   "scopes"
+  end
+
+  add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
+  add_index "oauth_access_tokens", ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
+  add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true
+
+  create_table "oauth_applications", force: true do |t|
+    t.string   "name",                      null: false
+    t.string   "uid",                       null: false
+    t.string   "secret",                    null: false
+    t.string   "redirect_uri", limit: 2048, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
