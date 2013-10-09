d568ee49c610a5573367157aa5147c8a47872360
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 21:06:07 +0800

    setup doorkeeper to make it work with Devise

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -5,10 +5,7 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    current_user || warden.authenticate!(:scope => :user)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
