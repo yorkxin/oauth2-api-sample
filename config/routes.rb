66c88d619df22698e38ae51fcec06d7788c68ea2
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 20:58:43 +0800

    generate doorkeeper files with `rails generate doorkeeper:install`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Oauth2ApiSample::Application.routes.draw do
+  use_doorkeeper
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
