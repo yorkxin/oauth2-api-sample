b2e0bfe6936b6fbc6992db42dde5b6e2c9a04a65
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 22:48:22 +0800

    Step 4.5: make the guard, which validates the access token and if valid, set current_user

--- app/api/concerns/api_guard.rb
+++ app/api/concerns/api_guard.rb
@@ -19,6 +19,52 @@ module APIGuard
 
   # Helper Methods for Grape Endpoint
   module HelperMethods
+    # Invokes the doorkeeper guard.
+    #
+    # If token string is blank, then it raises MissingTokenError.
+    #
+    # If token is presented and valid, then it sets @current_user.
+    #
+    # If the token does not have sufficient scopes to cover the requred scopes,
+    # then it raises InsufficientScopeError.
+    #
+    # If the token is expired, then it raises ExpiredError.
+    #
+    # If the token is revoked, then it raises RevokedError.
+    #
+    # If the token is not found (nil), then it raises TokenNotFoundError.
+    #
+    # Arguments:
+    #
+    #   scopes: (optional) scopes required for this guard.
+    #           Defaults to empty array.
+    #
+    def guard!(scopes: [])
+      token_string = get_token_string()
+
+      if token_string.blank?
+        raise MissingTokenError
+
+      elsif (access_token = find_access_token(token_string)).nil?
+        raise TokenNotFoundError
+
+      else
+        case validate_access_token(access_token, scopes)
+        when Oauth2::AccessTokenValidationService::INSUFFICIENT_SCOPE
+          raise InsufficientScopeError.new(scopes)
+
+        when Oauth2::AccessTokenValidationService::EXPIRED
+          raise ExpiredError
+
+        when Oauth2::AccessTokenValidationService::REVOKED
+          raise RevokedError
+
+        when Oauth2::AccessTokenValidationService::VALID
+          @current_user = User.find(access_token.resource_owner_id)
+
+        end
+      end
+    end
     private
     def get_token_string
       # The token was stored after the authenticator was invoked.
@@ -36,5 +82,24 @@ module APIGuard
   end
 
   module ClassMethods
+  end
+
+  #
+  # Exceptions
+  #
+
+  class MissingTokenError < StandardError; end
+
+  class TokenNotFoundError < StandardError; end
+
+  class ExpiredError < StandardError; end
+
+  class RevokedError < StandardError; end
+
+  class InsufficientScopeError < StandardError
+    attr_reader :scopes
+    def initialize(scopes)
+      @scopes = scopes
+    end
   end
 end
