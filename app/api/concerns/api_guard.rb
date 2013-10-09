77d87cf80d4a125192ee8ab98961ec0a50705b79
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 22:28:16 +0800

    Step 4.1: Install Bearer Token Middleware to fetch the token

--- app/api/concerns/api_guard.rb
+++ app/api/concerns/api_guard.rb
@@ -6,6 +6,14 @@ module APIGuard
   extend ActiveSupport::Concern
 
   included do
+    # OAuth2 Resource Server Authentication
+    use Rack::OAuth2::Server::Resource::Bearer, 'The API' do |request|
+      # The authenticator only fetches the raw token string
+
+      # Must yield access token to store it in the env
+      request.access_token
+    end
+
     helpers HelperMethods
   end
 
