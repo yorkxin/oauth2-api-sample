3e8f8277af593b2d093941a4e578577909e028c8
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-09 22:41:49 +0800

    Step 4.2: make a private method to get access token string that was previously fetched by Rack::OAuth2 authenticator

--- app/api/concerns/api_guard.rb
+++ app/api/concerns/api_guard.rb
@@ -19,6 +19,12 @@ module APIGuard
 
   # Helper Methods for Grape Endpoint
   module HelperMethods
+    private
+    def get_token_string
+      # The token was stored after the authenticator was invoked.
+      # It could be nil. The authenticator does not check its existence.
+      request.env[Rack::OAuth2::Server::Resource::ACCESS_TOKEN]
+    end
   end
 
   module ClassMethods
