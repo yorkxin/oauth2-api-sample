c9a7f13ca26e2f7c8e197bebfb0b58b5a8b5cf76
Yu-Cheng Chuang <ducksteven@gmail.com>
2013-10-10 16:48:41 +0800

    Step 5: Enable "Scopes"

--- app/api/v1/sample_api.rb
+++ app/api/v1/sample_api.rb
@@ -10,5 +10,15 @@ module V1
       guard!
       { :secret => "only smart guys can see this ;)" }
     end
+
+    get "top_secret" do
+      guard! scopes: [:top_secret]
+      { :top_secret => "T0P S3CR37 :p" }
+    end
+
+    get "choice_of_sg" do
+      guard! scopes: [:el, :psy, :congroo]
+      { :says => "El. Psy. Congroo." }
+    end
   end
 end
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -33,8 +33,9 @@ Doorkeeper.configure do
 
   # Define access token scopes for your provider
   # For more information go to https://github.com/applicake/doorkeeper/wiki/Using-Scopes
-  # default_scopes  :public
-  # optional_scopes :write, :update
+  default_scopes  :public
+  optional_scopes :top_secret,
+                  :el, :psy, :congroo
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
